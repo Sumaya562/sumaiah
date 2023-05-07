379fa9ab0fa868222c825e89ca3893385bd1b72c
Sumaya562 <127865625+Sumaya562@users.noreply.github.com>
2023-05-07 19:28:50 +0300

    Create sequences in the list.py

---
+++ sequences in the list.py
@@ -0,0 +1,97 @@
+EvenNumbers=[2,4,6,8,10,12,14,16]
+#print(EvenNumbers)
+#print(EvenNumbers[5])
+#print(EvenNumbers[-7])
+#print(EvenNumbers[-2])
+#print(EvenNumbers[-4])
+#print(EvenNumbers[6-11])
+#print(EvenNumbers*3)
+#for x in EvenNumbers:
+ #   print(x)
+#print(len(EvenNumbers)-1)
+#Months=["jan","Feb","Mar","April","May","jun","july","Aug"]
+#print(Months)
+#print(Months[4:6])
+#print(Months[:5])
+#fruits = ["apple", "banana", "cherry"]
+
+#fruits.insert(1,"orange")
+
+#print(fruits)
+#list=[1,2,3,4,5,6,7,8]
+#print(list[:])
+#print(list[2:-1])
+#print(list[0:-2])
+#print(3 in list)
+#print(9 in list)
+#print(3 not in list)
+#print(9  not in list)
+#list.append(10)
+#print(list)
+#print(list.index(7))
+#list.insert(5,9);
+#print(list)
+#list.sort();
+#print(list)
+#list.remove(3);
+#print(list)
+#list.reverse();
+#print(list)
+#del list[1:3]
+#print(list)
+#print(max(list))
+#print(min(list));
+#mylist=list.copy();
+#print(mylist)
+#sum=0;
+#aver=0;
+#for x in list:
+ #   sum=sum+x;
+  #  aver=sum/(len(list)-1);
+#print(sum);
+#print(aver)
+evenNumber=[0,2,4,6,8,10,12,14,16,18,20]
+evenNumber.append(22);
+evenNumber.append(24);
+evenNumber.append(26)
+#print(evenNumber)
+#print(evenNumber.index(8))
+#print(evenNumber.index(26))
+#print(evenNumber.index(30));
+#print(evenNumber[30])
+#evenNumber.insert(1,1);
+#evenNumber.insert(3,3);
+#evenNumber.insert(5,5);
+#evenNumber.insert(12,17)
+#evenNumber.insert(15,21)
+#evenNumber.insert(18,25)
+#evenNumber.insert(10,19)
+#evenNumber.sort();
+#num=[1,3,2,4,6,5];
+#num.sort();
+#num.remove(6)
+#num.remove(3)
+#num.reverse();
+#print(num)
+#del num[0:2];
+#del num[0:2]
+
+#print("Lowerst number in the list : ",min(num));
+#print("Lowerst number in the list even number : ",min(evenNumber));
+#print("Highest number in the list : ",max(num));
+#print("Highest number in the list even number : ",max(evenNumber))
+#myNewList=evenNumber;
+#print(myNewList);
+#mycopytlist=evenNumber.copy();
+#print("my copy list ",mycopytlist)
+Marks=[90,90,100,80,70];
+sum=0;
+aver=0;
+for x in Marks:
+    sum=sum+x;
+    aver= sum / len(Marks)
+print("isku dar dhaman madooyinka waa", sum);
+print("celceliska oo ardaykas ku baasay waa", aver);
+mytupe=(1,2,3,4,5,6)
+print(6 in mytupe)
+print(mytupe);
