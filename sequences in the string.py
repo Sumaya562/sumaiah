c99befbcfbd90c8d2a2377e090708f2f9e17b0a5
Sumaya562 <127865625+Sumaya562@users.noreply.github.com>
2023-05-07 19:32:10 +0300

    Create sequences in the string.py

---
+++ sequences in the string.py
@@ -0,0 +1,81 @@
+str="Eng Abdiaziz"
+print(str)
+# example of index error
+#str="Eng Abdiaziz"
+#print(str[14])
+#example od concatinating
+name= "Sumayya"
+last= "Mohamed"
+print(name+last)
+name="Ahmed"
+num="12345"
+print(name+num)
+#example of string
+name= "Abdi"
+print(name)
+#example of slice of string
+str="eng abdiaziz"
+print(str[0:3])
+str="eng abdiaziz"
+print(str[0:12])
+#example od in and not of string
+str1= "zahra"
+str2= "sumayya"
+print(str1 in str)
+str3= "mohamed"
+str4= "Abdi"
+print(str3 not in str4)
+#example of isalnum
+str= "abc123"
+print(str.isalnum())
+str= 'asc123#@'
+print(str.isalnum())
+#exsmple of isalpha
+str= "ahmed"
+print(str.isalpha())
+#example of is space
+str= "   "
+print(str.isspace())
+# example of is digit
+str= "123456"
+print(str.isdigit())
+# example isupper
+str= "ALI"
+print(str.isupper())
+#while
+str= "Ahmed"
+print(str.isupper())
+# example islower
+str="abdi"
+print(str.islower())
+# while
+str= "ABDI"
+print(str.islower())
+# example strip
+str="Ali"
+print(str.strip())
+# while
+text= ",,,@@@ welcome%%"
+print(text.strip(",@,%"))
+# example of end with
+str= "welcome with open hands"
+print(str.endswith("hands"))
+# or
+str= "welcome with open hands"
+print(str.endswith("s"))
+# example of start with
+str="welcome with open hands"
+print(str.startswith("welcome"))
+print(str.startswith("w"))
+# example of find , repets th index
+str= " welcome with open hands"
+print(str.find("with"))
+# example of replace
+str= "welcome with open hands"
+print(str.replace("welcome","ku soo dhawaw"))
+# example of repetition
+str= "welcome with open hands "
+print(str*3)
+# example od split
+x= str.split()
+print(x)
